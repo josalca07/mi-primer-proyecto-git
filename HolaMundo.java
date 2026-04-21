33d7e132f9e022f336fedc5b510efc3eb09dcda2
unknown <jsalcar877@g.educaand.es>
2026-04-21 16:08:11 +0200

    Modificado mensaje principal

--- HolaMundo.java
+++ HolaMundo.java
@@ -3,6 +3,6 @@ package daw1t;
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		System.out.println("Hola mundo");
+		System.out.println("Estoy aprendiendo control de versiones con Git");
 	}
 }
